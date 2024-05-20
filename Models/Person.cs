bce648290014a9c089c1825b08463f1ad867f4b9
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-05-20 03:46:05 +0300

    validation

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,20 +1,35 @@
-﻿using System.Reflection.Metadata;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace victors.Models
 {
     public class Person
     {
-       
+
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }= string.Empty;
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "village is required")]
+
         public string Village { get; set; } = string.Empty;
+        [Required(ErrorMessage = "District is required")]
+
         public string District { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Student contact is missing")]
+
         public string Contact { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Missing Nationality")]
+
         public string Nationality { get; set; } = string.Empty;
         public int Age { get; set; }
         public String Religion { get; set; }= string.Empty;
         public DateTime Birthdate { get; set; } = DateTime.Now.Date;
+        [Required(ErrorMessage = "Status missing")]
+
         public string Status { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         public string Email { get; set; }= string.Empty;
         public string Gender { get; set; } = string.Empty;
         public string RegistrationDate { get; set; } = $"{DateTime.Now.Day}/{DateTime.Now.Month}/{DateTime.Now.Year}";
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,13 +1,25 @@
-﻿namespace victors.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace victors.Models
 {
     public class Student:Person
     {
         public int StudentId { get;set; }
+        [Required(ErrorMessage = "Section  is required")]
+
         public string Section{ get; set; } = string.Empty;
+        [Required(ErrorMessage = "Grade is required")]
+
         public string Grade { get; set; }=string.Empty;
         public int Year { get; set; } = DateTime.Now.Year;
+        [Required(ErrorMessage = "Term is required")]
+
         public string Term { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Stream is required")]
+
         public string Stream { get; set; } = string.Empty;
+        [Required(ErrorMessage = "School Fees is required")]
+
         public decimal SchoolFees { get; set; } = 0M;
         public  string birthDateString { get; set; } = string.Empty;
         public decimal charge { get; set; } = 0M;
