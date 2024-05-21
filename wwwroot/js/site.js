278cda704ab57beeb9997feb49098af4f2b97136
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-05-21 13:14:22 +0300

    login rights

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,13 +18,36 @@ document.addEventListener('click', e => {
     })
 })
 
+
+
+
+document.addEventListener('click', e => {
+    const isDropDownButton = e.target.matches('[data-dropdown-vistor-button]')
+    if (!isDropDownButton && e.target.closest('[data-dropdown-visitor]') != null) return
+    let currentDropdown
+
+    if (isDropDownButton) {
+        currentDropdown = e.target.closest('[data-dropdown-visitor]')
+        currentDropdown.classList.toggle('active')
+    }
+    document.querySelectorAll('[data-dropdown-visitor].active').forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
+
 const bar = document.getElementById('bars');
 const menu = document.getElementById('header')
+const menuVistors = document.getElementById('header-vistor')
+
 const dropDownLists = document.getElementsByClassName('dropdown-menu')
+function toggleVistorHeader() {
+    menuVistors.classList.toggle('show')
+}
+
 
 function toggleHeader() {
-    menu.classList.toggle('active')
-    
+    menu.classList.toggle('active')    
 }
 
 function payUniform() {
