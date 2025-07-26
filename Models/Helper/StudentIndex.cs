484dcad2e35b24e8a79c356892d4c3c568449594
yusuf Nviiri <yusufnviiri@gmail.com>
2025-07-26 19:55:55 +0300

    code

--- Models/Helper/StudentIndex.cs
+++ Models/Helper/StudentIndex.cs
@@ -4,5 +4,16 @@
     {
         public ICollection<Student> Students { get; set; }=new List<Student>();
         public LookUpStudents LookUpStudents { get; set; } = new LookUpStudents();
+        public List<Student> PagedStudents { get; set; } = new();
+        public List<string> AllGrades { get; set; } = new();
+
+        public string SearchTerm { get; set; }
+        public string GradeFilter { get; set; }
+
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasNextPage => PageNumber < TotalPages;
+        public bool HasPreviousPage => PageNumber > 1;
     }
 }
